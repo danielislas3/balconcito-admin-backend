e60fa8836f43de956f48a9c8f604840cdf7e1380
danielislas3 <danielislas3@live.com>
2025-12-03 21:40:19 -0600

    feat: add payron contoller and migrations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :payment_methods, dependent: :destroy
   has_many :reimbursements_received, class_name: 'Reimbursement', foreign_key: 'to_user_id', dependent: :restrict_with_error
   has_many :reimbursements_created, class_name: 'Reimbursement', foreign_key: 'user_id', dependent: :restrict_with_error
+  has_one :payroll_employee, dependent: :destroy
 
   # Validations
   validates :name, presence: true
